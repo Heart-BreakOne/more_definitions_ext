0a0fe25043aa45a369388d627510d355c43c313d
Leonardo Luiz <lcoutinholuiz@gmail.com>
2024-11-23 23:56:10 -0300

    Fixing broken query selector

--- content_script.js
+++ content_script.js
@@ -1,5 +1,7 @@
 const observer = new MutationObserver(() => {
-    document.querySelectorAll('.S8ee5.CwbYXd.wHYlTd.vk_arc').forEach(el => {
+    
+    //document.querySelectorAll('.S8ee5.CwbYXd.wHYlTd.vk_arc').forEach(el => {
+        document.querySelectorAll('.S8ee5.CwbYXd.vk_arc').forEach(el => {
         if (el.innerText.trim() === "More definitions" && !el.disabled && el.offsetParent !== null) {
             el.click();
         }
